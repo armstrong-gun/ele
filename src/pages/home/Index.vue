8a1bf4f4cc988b34e39ffdcef3419da50bfb7809
lybycm <39153388+lybycm@users.noreply.github.com>
2018-08-07 15:54:49 +0800

    bannerSwiperCreate1.1

--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -1,11 +1,19 @@
 <template>
-    
+    <page id="home">
+        <!-- 轮播图 -->
+        <Banner></Banner>
+
+    </page>
 </template>
 
 <script>
+//引入轮播组件
+import Banner from '@/components/home/index/Banner.vue'
 export default {
-
-}
+    components:{
+        Banner
+    }
+}    
 </script>
 
 <style>
